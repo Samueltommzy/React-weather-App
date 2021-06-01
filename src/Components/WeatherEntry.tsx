395eb848c8b1a9e42d15ee28bebdfeeeddc2cb5e
Samuel Ologunowa <samueloluwatommzy@gmail.com>
2021-06-01 20:29:00 +0100

    weather data component

---
+++ src/Components/WeatherEntry.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import {Weather} from '../Models/Weather';
+import {getIconUrl} from '../Services/WeatherService';
+
+interface weatherProps {
+  weather: Weather | null;
+}
+export const WeatherEntry:FC<weatherProps> = ({weather}) => {
+  if(!weather){
+    return null;
+  }
+  return (
+    <div>
+      {new Date(weather.dt *1000).toDateString()}
+      <div>
+      <strong>{weather.main.temp}°C</strong>
+      <div>({weather.main.temp_min}°C/{weather.main.temp_max}°C)</div>
+      </div>
+      <div>Humidity: {weather.main.humidity}%</div>
+      {
+        weather.weather.map(d=><div key = {d.id}>
+          <img src = {getIconUrl(d.icon)} alt = {d.main}/>
+          {d.main} {d.description}
+        </div>)
+      }
+      <div>Feels like: {weather.main.feels_like}°C</div>  
+    </div>
+  )
+}
